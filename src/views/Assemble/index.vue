db4c2df068e9f3412464efd3b266bb6e75a18fc7
jinghanghe <heyuze@jinghang.ai>
2019-11-08 22:22:09 +0800

    修改cookie1

--- src/views/Assemble/index.vue
+++ src/views/Assemble/index.vue
@@ -28,31 +28,40 @@
                 localStorage.setItem("token")
               }
               if(this.getCookie("token")){
-                localStorage.getItem("token")
+                localStorage.setItem("token")
               }
-              
+
           },
           methods:{
             handlerclick(){
               this.$router.push("/ass")
             }
           },
-           getCookie(name) {
-        //获取当前所有cookie
-        var strCookies = document.cookie;
-        //截取变成cookie数组
-        var array = strCookies.split(';');
-        //循环每个cookie
-        for (var i = 0; i < array.length; i++) {
-            //将cookie截取成两部分
-            var item = array[i].split("=");
-            //判断cookie的name 是否相等
-            if (item[0] == name) {
-                return item[1];
+         
+       setCookie(name, value, seconds) {
+            seconds = seconds || 0;   //seconds有值就直接赋值，没有为0，这个根php不一样。
+            var expires = "";
+            if (seconds != 0 ) {      //设置cookie生存时间
+              var date = new Date();
+              date.setTime(date.getTime()+(seconds*1000));
+              expires = "; expires="+date.toGMTString();
             }
-        }
-        return null;
-       }
+            document.cookie = name+"="+escape(value)+expires+"; path=/";   //转码并赋值
+          },
+         getCookie(name) {
+            var nameEQ = name + '='
+            var ca = document.cookie.split(';') // 把cookie分割成组
+            for (var i = 0; i < ca.length; i++) {
+              var c = ca[i] // 取得字符串
+              while (c.charAt(0) == ' ') { // 判断一下字符串有没有前导空格
+                c = c.substring(1, c.length) // 有的话，从第二位开始取
+              }
+              if (c.indexOf(nameEQ) == 0) { // 如果含有我们要的name
+                return unescape(c.substring(nameEQ.length, c.length)) // 解码并截取我们要值
+              }
+            }
+            return false
+          }
 
         }
           
